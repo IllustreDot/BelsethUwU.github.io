bab121342837ec9a9ac01b8675def40c04dba748
jade.van_brabandt <jade.van_brabandt@etu.uca.fr>
2023-05-15 11:56:35 +0200

    feat : Better UI

--- talents/Priest/printOutTop.js
+++ talents/Priest/printOutTop.js
@@ -342,11 +342,11 @@ massiveReplaceString += "</td></tr>";
 
 massiveReplaceString += "</table>";
 
-massiveReplaceString += '<div><button onclick="copyURL()" style="width: 200px;height:50px;">Copy URL</button></div>';
+massiveReplaceString += '<div><button onclick="copyURL()" style="width: 200px;height:50px; background-color: #000; color: goldenrod;">Copy URL</button></div>';
 
-massiveReplaceString += '<br><span style = "color: #ffffff;"><span style = "font-weight: bold;">'+ textToSave +'</span></span><br>';
+massiveReplaceString += '<br><span style = "color: #ffffff;display:none;"><span style = "font-weight: bold;">'+ textToSave +'</span></span><br>';
 
-massiveReplaceString += '<span style = "color:ffd800;">'+ theURLtoCopy +'?tal=<span id = "copyURL" style = "color:ffd800"></span></span><br></div>';
+massiveReplaceString += '<span style = "color:ffd800; display:none;">'+ theURLtoCopy +'?tal=<span id = "copyURL" style = "color:ffd800"></span></span><br></div>';
 
 
 document.getElementById('replaceMeWithTalents').innerHTML = massiveReplaceString;
